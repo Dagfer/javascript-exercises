cb20c6f8145df9c91c8ef04824ec02ee75517825
Dagfer <137955962+Dagfer@users.noreply.github.com>
2023-11-22 17:15:15 -0500

    Pass the first test
    It is done.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(targetArray, ...values) {
+let index = targetArray.indexOf(...values);
+targetArray.splice(index, 1);
+return targetArray
 };
-
+//let minedArray = targetArray.splice(indexOf(...values), 1)
 // Do not edit below this line
 module.exports = removeFromArray;
