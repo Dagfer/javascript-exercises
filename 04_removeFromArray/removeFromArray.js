43039f514e0ca32baa6cf92e1b681a6c5ccceba7
Dagfer <137955962+Dagfer@users.noreply.github.com>
2023-11-27 21:23:32 -0500

    Complete tests
    All tests were completed but hit a genuine wall

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,12 @@
-const removeFromArray = function(targetArray, ...values) {
-let index = targetArray.indexOf(...values);
-targetArray.splice(index, 1);
+const removeFromArray = function(targetArray, ...targetValues) {
+   for (let i = 0 ; i < targetValues.length; i++){
+    let index = targetArray.indexOf(targetValues[i]);
+    if (index === -1) continue;
+    targetArray.splice(index, 1);
+
+   }
 return targetArray
+ 
 };
 //let minedArray = targetArray.splice(indexOf(...values), 1)
 // Do not edit below this line
