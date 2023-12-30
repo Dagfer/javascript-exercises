a7dfd5e3bf935492733f62b84bace5700519c914
Dagfer <137955962+Dagfer@users.noreply.github.com>
2023-12-30 17:38:02 -0500

    Work on exercise 5 and 6
    Stumped by 5, solved 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+  let integers = [];
+  let integerSum = 0
+  let i = firstNum;
+    do {
+      integers.push(i);
+      i++}
+  while (i <= lastNum);
 
-};
+  integers.forEach(x => {
+    integerSum += x;
+     });
+     return integerSum
+
+    }
+
+ 
+
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  let leap;
+    if (year % 100 === 0 && year % 400 !== 0 || year % 4 !== 0){
+        leap = false;
+} else {
+    leap = true;
+}
+return leap
 };
 
 // Do not edit below this line
