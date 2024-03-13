d5b6491fda69e412ae9dc9740e036373a3377599
Dagfer <137955962+Dagfer@users.noreply.github.com>
2024-03-12 20:52:30 -0400

    Pass the tests
    wrote code that passed all the tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const punctuation = ['.', ',', ';', ':', '!', '?', '"', "'", '(', ')', '[', ']', '{', '}', '-', '_'];
+    let newString = string.replaceAll(" ", "")
+            let cleanString = '';
+            for (let i = 0; i < newString.length; i++) {
+                if (!punctuation.includes(newString[i])) {
+                 cleanString += newString[i];
+                 stringArray = cleanString.toLowerCase().split('');
+                }
+            } 
+        
+            if (stringArray.toReversed().toString() === stringArray.toString()){
+                return true
+            } else return false;  
+            
+       
+    }
+   
+   
+     
+       
+     
 
-};
+;
 
 // Do not edit below this line
 module.exports = palindromes;
