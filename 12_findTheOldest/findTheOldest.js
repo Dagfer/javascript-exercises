0dc628748c4e440f6d747f49eb4727d710ebf5de
Dagfer <137955962+Dagfer@users.noreply.github.com>
2024-03-16 20:14:57 -0400

    Complete final exercise
    Did not hit a wall with this one yay

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
+ const ages = people.map((person) => {
+  if ("yearOfDeath" in person) {
+     return person.yearOfDeath - person.yearOfBirth}
+  
+  else {
+        
+       return currentYear - person.yearOfBirth
+      }})
+ ;
+ ages.sort((a, b) => b - a);
+ const oldest = people.find((person) => person.yearOfDeath - person.yearOfBirth === ages[0] || currentYear - person.yearOfBirth === ages[0]);
+  return oldest
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
