78d1a5627a2a0bd303d8598384dab991d062da1c
Dagfer <137955962+Dagfer@users.noreply.github.com>
2024-03-16 20:16:44 -0400

    Push missing exercises
    Frustration over fibo made me forgo commits

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(fiboIndex) {
+    if (fiboIndex < 0) return "OOPS";
+    const array = [0, 1];
+    for (i = 0; i <= fiboIndex; i++){
+      
+     fibNum = array[i] + array[i + 1]
+      array.push(fibNum)
+   }
+     return array[fiboIndex]
+   };
 
-};
 
 // Do not edit below this line
 module.exports = fibonacci;
